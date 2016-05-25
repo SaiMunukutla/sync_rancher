689ec351f0060e90dcfe0bec8541d3a7e865fe04
SusrithaMunukutla <saim@virtusa.com>
2016-05-25 22:11:52 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ main() {
   set -o pipefail
 
   case "$1" in
-  *)               sync_gateway https://gist.githubusercontent.com/SusrithaMunukutla/6a58c916b1498974ed3cbceb2d6d7301/raw/6385104d433689376d3c58d2190452fe9064f4b1/config.json;;
+  *)               sync_gateway https://gist.githubusercontent.com/SusrithaMunukutla/6a58c916b1498974ed3cbceb2d6d7301/raw/b9ae178637a3184a2096bc5092bcb131e32ac996/config.json;;
   esac
 }
 
